8eb7f0c3918e4275c656e1e91c6454ab7fab1f3b
Raquel Magnani <quel.magnani88@gmail.com>
2022-04-18 20:14:23 +0200

    add to-do-list components to app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'to-do-list';
+  tasks: any[] = [];
+  add($event) {
+    const task = { ...$event, isComplete: false };
+    this.tasks.push(task);
+  }
 }
